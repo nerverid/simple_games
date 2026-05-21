74c884c16c3326b2c4acccbea9254825b7a7c4e8
nerverid <nero-add@mail.ru>
2026-05-21 17:15:38 +0700

    added choise players

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -6,32 +6,48 @@ fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::rng().random_range(1..=101);
-
-    println!("The secret number is: {secret_number}");
-  
-    loop { 
-        println!("Please input your guess.");
-  
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
     
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => contunue,
-        };
+    println!("Please choose who guesses: you or the Computer");
+    println!("(I/C)");
+   
+    let mut choice = String::new();
+   
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
  
-        println!("You guessed: {guess}");
+    let choices = choice.chars().next().unwrap(); 
+    if choices == 'I' {
+        println!("Your choice: {choices}. You guess.");
+        println!("The secret number is: {secret_number}");
+  
+        loop { 
+            println!("Please input your guess.");
+  
+            let mut guess = String::new();
+
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Failed to read line");
     
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+ 
+            println!("You guessed: {guess}");
+    
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small!"),
+                Ordering::Greater => println!("Too big!"),
+                Ordering::Equal => {
+                    println!("You win!");
+                    break;
+                }
             }
-        }
-    } 
+        } 
+    } else if choices == 'C' {
+        println!("Your choice: {choices}. The computer guesses.");
+    }
+
 }
